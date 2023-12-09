e3e6ae9b50c9dec1e46b861e9f7bb3aa32cc993c
amarcus <amarcus@ucvts.org>
2023-12-09 16:02:26 -0500

    Fixed font plus timer.

--- script/script.js
+++ script/script.js
@@ -4,11 +4,11 @@ console.log('the file loaded lmao');
 const update = () => {
     time = Math.round((time - 0.01) * 100) / 100;
     if ((time * 100) % 100 == 0) {
-        document.getElementById("countdownText").innerHTML = time + ".00";
+        document.getElementById("countdownText").innerHTML = "00:00:" + time + ".00";
     } else if ((time * 100) % 10 == 0) {
-        document.getElementById("countdownText").innerHTML = time + "0";
+        document.getElementById("countdownText").innerHTML = "00:00:" + time + "0";
     } else {
-        document.getElementById("countdownText").innerHTML = time;
+        document.getElementById("countdownText").innerHTML = "00:00:" + time;
     }
     if (time <= 0) {
         clearInterval(i);
